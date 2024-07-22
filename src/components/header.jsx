e0009f25f2d52d111ed22412fffacc7bdba7a692
JamesChristopher0515 <aaavip0515@gmail.com>
2024-07-21 17:46:00 -0700

    updated1

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const Header = (props) => {
   return (
-    <header id="header">
+    <header id="header" style={{paddingTop: "80px"}}>
       <div className="intro">
         <div className="overlay">
           <div className="container">
